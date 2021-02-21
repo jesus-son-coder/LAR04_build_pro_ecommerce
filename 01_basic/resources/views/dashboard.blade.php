0d4b31274224c0c93eb12779cc58319433f53bc1
hseka <sekaherve@gmail.com>
2021-02-21 19:54:58 +0100

    Identification de la zone de contenu textuel du Dashboard, en mode connecté

--- 01_basic/resources/views/dashboard.blade.php
+++ 01_basic/resources/views/dashboard.blade.php
@@ -8,9 +8,13 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            <!-- C'est ci que se trouve la bloc visuel de la homepage du Dashboard en mode connecté.
+                Vous pouvez le supprimer pour le personnaliser ! -->
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-jet-welcome />
             </div>
+            <!-- fin du bloc visuel de la homepage du Dashboard -->
         </div>
     </div>
 </x-app-layout>
