b45a4fcaf2cc1ed59f61493a55d388f941af8dd4
hseka <sekaherve@gmail.com>
2021-02-21 19:49:55 +0100

    Rajouter un Lien (de test) dans le Menu -- en mode connecté

--- 01_basic/resources/views/navigation-menu.blade.php
+++ 01_basic/resources/views/navigation-menu.blade.php
@@ -15,6 +15,11 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+
+                    <!-- rajouter un autre lien : -->
+                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('Dashboard') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
