9a307e560749656afbb5c4e67ae44d12758f126e
hseka <sekaherve@gmail.com>
2021-02-21 22:18:52 +0100

    Insérer un Tableau statique dans le Dashboard

--- 01_basic/resources/views/dashboard.blade.php
+++ 01_basic/resources/views/dashboard.blade.php
@@ -12,6 +12,50 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg" style="margin-bottom:10px;padding:30px;">
                 <h1>Hello Hervé ;)</h1>
+
+                <style>
+                    .dashboard-table-container {
+                        padding: 1.5rem;
+                        border: solid #dee2e6;
+                        border-width: 1px;
+                        border-top-left-radius: .25rem;
+                        border-top-right-radius: .25rem;
+                        margin-top: 25px;
+                    }
+                </style>
+
+                <div class="dashboard-table-container">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </div>
 
